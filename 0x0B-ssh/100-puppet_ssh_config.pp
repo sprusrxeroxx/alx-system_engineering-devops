749853ab29e65f8c7feae2f500633631c16798cc
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-14 19:05:10 +0200

    chore: Update puppet configuration for SSH

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,9 @@
 #This is a script to disable password authentication on my ssh server
 
 class { 'ssh' :
-  # Disable password authentication
   ChallengeResponseAuthentication => 'no',
 
-  # Configure client options for specific hosts
   Host { '*':
-    # Use the private key located at ~/.ssh/school
-    IdentityFile => '/home/user/.ssh/school',  # Replace 'user' with your actual username
-    # Additional options can be specified here if needed
+    IdentityFile => '~/.ssh/school',
   }
 }
