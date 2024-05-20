76b91e062750ba30acdd32c99741806e63d82975
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-21 00:41:44 +0200

    feat: Add script to gather TODO list progress for a given employee ID

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+""""
+Python script that, using this REST API, for a given employee ID, 
+returns information about his/her TODO list progress.
+"""
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    url = 'https://jsonplaceholder.typicode.com/users'
+    response = requests.get(url)
+    data = response.json()
+    user_id = sys.argv[1]
+    user = [user for user in data if user.get('id') == int(user_id)][0]
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    response = requests.get(url)
+    data = response.json()
+    tasks = [task for task in data if task.get('userId') == int(user_id)]
+    completed = [task for task in tasks if task.get('completed')]
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get('name'), len(completed), len(tasks)))
+    [print('\t {}'.format(task.get('title'))) for task in completed]
