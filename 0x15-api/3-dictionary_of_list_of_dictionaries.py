5388e449a2d08d62b5a822a20ec375a7f2aa35f6
ntokozo vilakazi <ntokozovilakazi36@gmail.com>
2024-05-21 03:27:53 +0200

    chore: pycodestyle adherence

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,8 +3,8 @@
 Python script that exports all employee task data
 from an API in JSON format.
 """
-import requests
 import json
+import requests
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/users'
